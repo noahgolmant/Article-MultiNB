032b17672f2eecb153aa29d2f0776d148b0b68cc
Noah Golmant <noah.golmant@gmail.com>
2015-05-04 21:23:27 -0400

    Added tester to evaluate accuracy, including multinomialNB and simple SVM. TODO test SVR, regressive multinomialNB, and parameter tuning; create semi-supervised learner via web crawl on initially trained classifier

--- com/noahgolmant/ArticleSVR/MultinomialNB.py
+++ com/noahgolmant/ArticleSVR/MultinomialNB.py
@@ -11,10 +11,14 @@ from newspaper import Article
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from nltk.stem.snowball import SnowballStemmer
 import numpy
 
 name_to_index = { "conservative":0, "unbiased":1, "liberal":2}
-    
+stop = stopwords.words('english') 
+stemmer = SnowballStemmer("english")
     
 def article_text_generator(lines):
     for line in lines:
@@ -25,7 +29,12 @@ def get_article_text(url):
     article = Article(url)
     article.download()
     article.parse()
-    return article.text
+    
+    # filter out stop words
+    # and store the stemmers
+    stop_word_filtered_text = [stemmer.stem(w) for w in article.text.split() if w.lower() not in stop]
+    
+    return ' '.join(stop_word_filtered_text)
     
 
 if __name__ == '__main__':
@@ -33,28 +42,19 @@ if __name__ == '__main__':
     corpus = numpy.load("corpus.npy")
     target = numpy.load("training_target.npy")
     
-    # input corpus into vectorizer
-    print "Vectorizing training data.."
-    vectorizer = CountVectorizer(min_df=1)
-    X_training = vectorizer.fit_transform(corpus)
-    
-    print "Applying term-frequency inverse-document-frequency transform to training data..."
-    # apply term-frequency x inverse document frequency transformer
-    tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_training)
-    
-    print "Training Multinomial Naive Bayes Classifier around data..."
-    clf = MultinomialNB().fit(X_train_tfidf, target)
-  
+    text_clf = Pipeline([('vect', CountVectorizer(min_df=1)), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+    text_clf = text_clf.fit(corpus, target)
+
     new_url = ""
     while new_url is not "quit":
         new_url = raw_input("Enter URL to classify: " )
         new_text = get_article_text(new_url)
-        X_new = vectorizer.transform([new_text])
+        #X_new = vectorizer.transform([new_text])
         
-        X_new_tfidf = tfidf_transformer.transform(X_new)
+        #X_new_tfidf = tfidf_transformer.transform(X_new)
         
-        predicted = clf.predict(X_new_tfidf)
+        #predicted = clf.predict(X_new_tfidf)
+        predicted = text_clf.predict([new_text])
         print "Predicted category: {}".format(str(predicted[0]))
         
     
---
+++ com/noahgolmant/ArticleSVR/Tester.py
@@ -0,0 +1,65 @@
+'''
+Created on May 4, 2015
+
+@author: Noah
+'''
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfTransformer
+from MultinomialNB import article_text_generator, get_article_text
+from sklearn import svm
+from sklearn.pipeline import Pipeline
+import numpy
+
+
+name_to_index = { "conservative":0, "unbiased":1, "liberal":2}
+# used for loading training data into classifier
+print "Loading corpus"
+corpus = numpy.load("corpus.npy")
+target = numpy.load("training_target.npy")
+
+# training data loading from file
+print "Loading test article data"
+test_data = open("test_articles.csv")
+lines = [line.strip() for line in test_data.readlines()]
+testList = []
+testActualCategories = []
+
+counter = 0
+print "Analyzing test data"
+for line in lines:
+    actual_category, url = line.split(",")
+    testList.append(url)
+    testActualCategories.append(actual_category)
+    #target.append(category_index)
+    counter = counter + 1
+    #update_progress(counter, len(lines))
+
+correct = 0.0
+numTests = float(len(testList))
+
+classifier_type = raw_input("Choose classifier type: MultinomialNB (1), SVM (2): ")
+if classifier_type == "1":
+
+    clf = Pipeline([('vect', CountVectorizer(min_df=1)), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+    clf=  clf.fit(corpus, target)
+    for url, actual_category in zip(testList, testActualCategories):
+        new_text = get_article_text(url)
+        predicted = clf.predict([new_text])
+        if predicted == name_to_index[actual_category]:
+            correct += 1.0
+    
+    print "Multinomial accuracy: {}".format(str(100.0*correct/numTests)[:4])
+elif classifier_type == "2":
+    clf = Pipeline([('vect', CountVectorizer(min_df=1)), ('tfidf', TfidfTransformer()), ('clf', svm.SVC())])
+    clf=  clf.fit(corpus, target)
+    for url, actual_category in zip(testList, testActualCategories):
+        new_text = get_article_text(url)
+        predicted = clf.predict([new_text])
+        if predicted == name_to_index[actual_category]:
+            correct += 1.0
+            
+    print "SVM accuracy: {}".format(str(100.0*correct/numTests)[:4])
+else:
+    print "Invalid entry"
